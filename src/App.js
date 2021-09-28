b23d2422d86e302582fce2a8dd05e3727cb5c0e3
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-09-28 18:17:02 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './Components/NavigationBar';
+import MyFooter from './Components/MyFooter'
+import Welcome from './Components/Welcome'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LatestReleases from './Components/LatestReleases';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <NavigationBar />
+      <Welcome />
+      <LatestReleases />
+      <MyFooter />
     </div>
-  );
+  )
 }
 
 export default App;
