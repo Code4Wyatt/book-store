628b6d3f326e94274ed6056b125d1e9455eda5ee
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-09-30 16:59:35 +0100

    message

--- src/Components/CommentArea.jsx
+++ src/Components/CommentArea.jsx
@@ -13,7 +13,7 @@ class CommentArea extends React.Component {
   componentDidMount = async () => {
     try {
       let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/comments/this.id",
+        "https://striveschool-api.herokuapp.com/api/comments/",
         {
           headers: {
             "Content-type": "application/json",
@@ -21,40 +21,43 @@ class CommentArea extends React.Component {
               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MzMwMDUyOTQsImV4cCI6MTYzNDIxNDg5NH0.d_KfdGYMLP2cIvkIVTXXAEEsTnwwgbKEKlTIzOAU9uA",
           },
         }
-        );
-        if (response.ok) {
-            let data = await response.json()
-            console.log(data)
-            this.setState({
-              reservations: data,
-              isLoading: false,
-              isError: false
-            })
-        }
+      );
+      if (response.ok) {
+        let data = await response.json()
+        console.log(data)
+        this.setState({
+          reservations: data,
+          isLoading: false,
+          isError: false
+        })
+      }
     } catch (err) {
       console.log(err.message);
       this.setState({
-              isLoading: false,
-              isError: false
+        isLoading: false,
+        isError: false
+      })
     }
-  };
-
-  render() {
-    return (
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <p>Modal body text goes here.</p>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary">Close</Button>
-          <Button variant="primary">Save changes</Button>
-        </Modal.Footer>
-      </Modal.Dialog>
-    );
   }
 
+    render() {
+      return (
+        <Modal.Dialog >
+          <Modal.Header closeButton>
+            <Modal.Title>Comments</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <p>{this.state.comments}</p>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="secondary">Close</Button>
+            <Button variant="primary">Save changes</Button>
+          </Modal.Footer>
+        </Modal.Dialog >
+      )
+    };
+  };
+
+export default CommentArea;
