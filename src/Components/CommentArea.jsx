6beba9a0ed0a924d3f83862adafb06e93c85f3c0
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-09-30 17:09:02 +0100

    message

--- src/Components/CommentArea.jsx
+++ src/Components/CommentArea.jsx
@@ -5,15 +5,25 @@ import Button from 'react-bootstrap/Button';
 class CommentArea extends React.Component {
 
     state = {
-      comments: [],
-      isLoading: true,
-      isError: false
+      comments: "",
+      
     }
 
+  //How to pass the server data to the children components
+    // you are going to to a request
+    // if the request is ok you need to store this data in a component STATE
+    // if the component STATE has 1 element or more you want to display the element
+
+    // How to show/display the data 
+    // from the State you are going to perform a map
+    // this.state.booksComments.map(book=> <Comment key={book._id} book={book}/>)
+
+    // This fetches the comments
+  
   componentDidMount = async () => {
     try {
       let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/comments/",
+        "https://striveschool-api.herokuapp.com/api/comments/" + this.props.asin,
         {
           headers: {
             "Content-type": "application/json",
@@ -26,9 +36,8 @@ class CommentArea extends React.Component {
         let data = await response.json()
         console.log(data)
         this.setState({
-          reservations: data,
-          isLoading: false,
-          isError: false
+          comments: data,
+          
         })
       }
     } catch (err) {
